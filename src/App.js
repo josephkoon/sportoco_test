f02cf26cbf99677c937d9768ddd99e68a82b3824
Joseph Koon <josephandrewkoon@gmail.com>
2018-08-14 21:45:51 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import Transition from "react-transition-group/Transition";
+import TransitionGroup from "react-transition-group/TransitionGroup";
+
+
+
+
 
 class App extends Component {
   constructor(){
@@ -15,38 +21,39 @@ class App extends Component {
 
   render() {
 
-    var yellow = {backgroundColor:'yellow', borderRadius:'50px 50px 50px 50px'}
+    var yellow = {backgroundColor:'yellow', borderRadius:'50px 50px 50px 50px', transition: "transform 2s ease", width: "100%", height: "100%" }
     var noYellow = {}
 
 
     return (
       <div>
 
-
         <div style={{cursor:'pointer', zIndex:'3', left:'460px', top:'146px', textAlign:'center', border:'4px solid black', position:'absolute', borderRadius:'50%', height:'60px', width:'60px', backgroundColor:'#2C8B42', background: 'linear-gradient(180deg, #2C8B42 50%, #206030 50%)'}}>
           <strong style={{fontSize:'46px', color:'white'}}>J</strong>
         </div>
 
-        <div style={{ opacity:'.5', zIndex:'0', left:'434px', top:'120px', textAlign:'center', position:'absolute', borderRadius:'50%', height:'120px', width:'120px', backgroundColor:'black' }}>
+        <div className="pulse-button" style={{ opacity:'.5', zIndex:'0', left:'434px', top:'120px', textAlign:'center', position:'absolute', borderRadius:'50%', height:'120px', width:'120px', backgroundColor:'black' }}>
         </div>
 
-        <div style={{zIndex:'2', position:'absolute', top:'75px', left:'25px', }}>
-          <div onClick={()=>this.select(0)} className='btn' style={{textAlign:'center', borderRadius:'50px 0 0 50px', fontSize:'36px', display:'inline-block', width:'320px', height:'100px', lineHeight:'100px', backgroundColor:'white'}}>
-          <div style={this.state.selected == 0 ? yellow : noYellow}>
-            <strong>OUT/BOUNDS</strong>
+        <div style={{cursor:'pointer', zIndex:'2', position:'absolute', top:'75px', left:'25px', backgroundColor:'white', borderRadius:'50px 50px 50px 50px' }}>
+          <div style={{position: 'absolute', borderRadius:'50px 50px 50px 50px', width: "320px", height: "100%", backgroundColor: 'yellow', transition: "transform 1.5s ease", zIndex: -1 }} className={ this.state.selected === 1 ? "test" : this.state.selected === 2 ?  "test1": "test2"} >
           </div>
+          <div onClick={()=>this.select(0)} className='btn' style={{textAlign:'center', borderRadius:'50px 0 0 50px', fontSize:'36px', display:'inline-block', width:'320px', height:'100px', lineHeight:'100px'}}>
+            <div style={{zIndex:5}}>
+              <strong>OUT/BOUNDS</strong>
+            </div>
           </div>
-
-          <div className='btn' style={{textAlign:'center', fontSize:'36px', display:'inline-block', width:'320px', height:'100px', lineHeight:'100px',  backgroundColor:'white'}}>
-          <div onClick={()=>this.select(1)} style={this.state.selected == 1 ? yellow : noYellow}>
-            <strong>TOUCHBACK</strong>
+          
+          <div style={{textAlign:'center', fontSize:'36px', display:'inline-block', width:'320px', height:'100px', lineHeight:'100px'}}>
+            <div onClick={()=>this.select(1)} >
+              <strong>TOUCHBACK</strong>
+            </div>
           </div>
-          </div>
-
-          <div className='btn' style={{textAlign:'center', borderRadius:'0 50px 50px 0',  fontSize:'36px', display:'inline-block', width:'320px', height:'100px', lineHeight:'100px',  backgroundColor:'white'}}>
-          <div onClick={()=>this.select(2)} style={this.state.selected == 2 ? yellow : noYellow}>
-            <strong>RETURN</strong>
-          </div>
+          
+          <div className='btn' style={{textAlign:'center', borderRadius:'0 50px 50px 0',  fontSize:'36px', display:'inline-block', width:'320px', height:'100px', lineHeight:'100px'}}>
+            <div onClick={()=>this.select(2)} >
+              <strong>RETURN</strong>
+            </div>
           </div>
         </div>
 
